03b028b718fc232f7c4094722e69944300bb7fbb
Arise Damilare <arisedamilare5@gmail.com>
2024-02-02 15:06:12 +0100

    Overwriting the previous file

---
+++ backend/swiftsuit/swiftsuit/accounts/urls.py
@@ -0,0 +1,27 @@
+# from django.urls import re_path
+# from . import views as vw
+
+# urlpatterns = [
+#     re_path('login/', vw.login),
+#     re_path('signup/', vw.signup),
+#     re_path('test_token/', vw.test_token),
+    
+# ]
+
+##############################################
+
+from django.urls import path
+from .views import RegisterUserView, VerifyUserEmail, LoginUserView, PasswordResetView,PasswordResetConfirm,SetNewPassword, LogoutUserView
+
+
+
+urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name="register"),
+    path('verify_email/', VerifyUserEmail.as_view(), name='verify_email'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('set_new_password/', SetNewPassword.as_view(), name='set_new_password'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
+    
+]
--- backend/swiftsuite/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from accounts import views as view
-
-urlpatterns = [
-    path('register/', view.register_user, name='register'),
-    path('login/', view.user_login, name='login'),
-    path('logout/', view.user_logout, name='logout'),
-    path('change_password/', view.change_password, name='change_password'),
-]
