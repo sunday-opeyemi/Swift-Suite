d11d03af50dd38396f77b009ba0f58fa988f8412
Sunday Opeyemi <sunday.opeyemi2018@gmail.com>
2024-02-02 14:06:01 +0100

    The have created an index page to test

--- backend/swiftsuite/swiftsuite/urls.py
+++ backend/swiftsuite/swiftsuite/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('accounts.urls')),  # Include the app's URLs
+    path('', TemplateView.as_view(template_name='index.html'), name="home"),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
