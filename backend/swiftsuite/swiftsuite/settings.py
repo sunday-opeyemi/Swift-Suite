a31495b831dd6662162f59b4bac868760c545415
Arise Damilare <arisedamilare5@gmail.com>
2024-02-05 17:02:32 +0100

    02-05-24

--- backend/swiftsuite/swiftsuite/settings.py
+++ backend/swiftsuite/swiftsuite/settings.py
@@ -77,9 +77,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'swiftsuite_db',
+        'NAME': 'swiftsuit2_db',
         'USER': 'root',
-        'PASSWORD': 'yemi1234',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',                     
         'PORT': '3306',
         'OPTIONS': {
@@ -89,14 +89,20 @@ DATABASES = {
 }
 
 
-# Email Backend Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Replace with your preferred backend
-EMAIL_PORT = 587  # Replace with your email port
-EMAIL_USE_TLS = True  # Set to False if your email server doesn't use TLS
-EMAIL_HOST = 'your_email_host'  # Replace with your email host for gmail -> 'smtp.gmail.com'
-EMAIL_HOST_USER = 'your_email_username'  # Replace with your email username
-EMAIL_HOST_PASSWORD = 'your_email_password'  # Replace with your email password
 
+# # Email Backend Configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Replace with your preferred backend
+# EMAIL_PORT = 587  # Replace with your email port
+# EMAIL_USE_TLS = True  # Set to False if your email server doesn't use TLS
+# EMAIL_HOST = 'your_email_host'  # Replace with your email host for gmail -> 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'your_email_username'  # Replace with your email username
+# EMAIL_HOST_PASSWORD = 'your_email_password'  # Replace with your email password
+
+
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER = '16a3aa2f086189'
+EMAIL_HOST_PASSWORD = 'a680d6de32a1be'
+EMAIL_PORT = '2525'
 
 
 # Password validation
