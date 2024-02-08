925cf3ab86f0195f672d56f521833da585a5f387
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-02-08 15:12:24 +0100

    form validation.

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -41,9 +41,9 @@ const SignUp = () => {
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/
   let formik = useFormik({
     initialValues: {
-      name: "",
+      firstname: "",
+      lastname: "",
       email: "",
-      username: "",
       password: "",
       confirm: ""
     },
@@ -51,11 +51,11 @@ const SignUp = () => {
         console.log(values);
     },
     validationSchema: yup.object({
-      name: yup.string().required('Field is required'),
+      firstname: yup.string().required('Field is required'),
+      lastname: yup.string().required( <span className='flex'><span>Field is required</span></span> ),
       email: yup.string().email('Invalid email format').required( <span className='flex'><span>Field is required</span></span> ),
-      username: yup.string().required( <span className='flex'><span>Field is required</span></span> ),
       password: yup.string().required(<span className="flex">{' '}<span>Field is required</span></span>).matches(passwordRegex, 'Password must include letters and numbers'),
-      confirm: yup.string().required(<span className="flex">{' '}<span>Confirm your Password</span></span> ),
+      confirm: yup.string().required(<span className="flex">{' '}<span>Confirm your Password</span></span> ).oneOf([yup.ref('password'), null], 'Passwords do not match'),
     })
   });
   return (
@@ -71,32 +71,32 @@ const SignUp = () => {
               <h1 className='text-center font-semibold text-xl text-[#089451]'>Create an Account</h1>
               <form action="" className='' onSubmit={formik.handleSubmit}>
               <p className='flex my-4 font-semibold text-xl text-[#089451]'>Sign Up</p>
-                <div className=' h-[80px] relative'>
-                  <label htmlFor="" className='font-semibold'>Name</label><br />
-                  <input type="text"  className='px-4 py-2 w-full border-2  border-[#089451] focus:outline-[#089451]' name='name' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                  <span className='text-red-500 font-bold'>{formik.touched.name && formik.errors.name}</span>
+                <div className=' h-[80px] my-1'>
+                  <label htmlFor="" className='font-semibold my-1'>Firstname</label><br />
+                  <input type="text" placeholder='Jane'  className='px-4 py-2 w-full border-2 mt-1 border-[#089451] focus:outline-[#089451]' name='firstname' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
+                  <span className='text-red-500 my-1'>{formik.touched.firstname && formik.errors.firstname}</span>
                   {/* <span className='absolute top-[35px] right-5 text-red-700'>{formik.touched.name ? <MdErrorOutline /> : ''}</span> */}
                 </div>
-                <div className='h-[80px]'>
-                  <label htmlFor="Email" className='font-semibold'>Email</label><br />
-                  <input  type="email"  className='px-4 py-2 w-full border-2 border-[#089451]  focus:outline-[#089451]' name='email' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                  <span className='text-red-500 font-bold'>{formik.touched.email && formik.errors.email}</span>
+                <div className='h-[80px] my-1'>
+                  <label htmlFor="" className='font-semibold my-1'>Lastname</label><br />
+                  <input  type="text" placeholder='Doe'   className='px-4 py-2 w-full border-2 mt-1 border-[#089451] focus:outline-[#089451]' name='lastname' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
+                  <span className='text-red-500 my-1'>{formik.touched.lastname && formik.errors.lastname}</span>
                 </div>
-                <div className='h-[80px]'>
-                  <label htmlFor="" className='font-semibold'>Username</label><br />
-                  <input  type="text"   className='px-4 py-2 w-full border-2 border-[#089451] focus:outline-[#089451]' name='username' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                  <span className='text-red-500 font-bold'>{formik.touched.username && formik.errors.username}</span>
+                <div className='h-[80px] my-1'>
+                  <label htmlFor="Email" className='font-semibold my-1'>Email</label><br />
+                  <input  type="email" placeholder='jane@gmail.com'  className='px-4 py-2 w-full border-2 mt-1 border-[#089451]  focus:outline-[#089451]' name='email' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
+                  <span className='text-red-500 my-1'>{formik.touched.email && formik.errors.email}</span>
                 </div>
-                <div className='h-[80px] relative'>
+                <div className='h-[80px] relative my-1'>
                   <label htmlFor="" className='font-semibold'>Password</label><br />
-                  <input  type={passwordVisible ? 'text' : 'password'}   autoComplete='off' className='border-2 border-[#089451] py-2 px-4 w-full focus:outline-[#089451]' name='password' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                  <span className='text-red-500 font-bold'>{formik.touched.password && formik.errors.password}</span>
+                  <input  type={passwordVisible ? 'text' : 'password'} placeholder='**********'   autoComplete='off' className='border-2 mt-1 border-[#089451] py-2 px-4 w-full focus:outline-[#089451]' name='password' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
+                  <span className='text-red-500 my-1'>{formik.touched.password && formik.errors.password}</span>
                   <span onClick={() => togglePasswordVisibility('password')} className='absolute top-[35px] right-5'>{!passwordVisible ? <IoEyeSharp /> :  <BsEyeSlashFill /> }</span>
                 </div>
-                <div className='h-[80px] relative'>
+                <div className='h-[80px] relative my-1'>
                   <label htmlFor="" className='font-semibold'>Confirm Password</label><br />
-                  <input type={confirmVisible ? 'text' : 'password'}  className='border-2 bg-white border-[#089451] py-2 px-4 w-full focus:outline-[#089451]' name='confirm' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
-                  <span className='text-red-500 font-bold'>{formik.touched.confirm && formik.errors.confirm}</span>
+                  <input type={confirmVisible ? 'text' : 'password'} placeholder='**********'  className='border-2 bg-white border-[#089451] py-2 mt-1 px-4 w-full focus:outline-[#089451]' name='confirm' onBlur={formik.handleBlur} onChange={formik.handleChange}/>
+                  <span className='text-red-500 my-1'>{formik.touched.confirm && formik.errors.confirm}</span>
                   <span onClick={() => togglePasswordVisibility('confirm')} className='absolute top-[35px] right-5'>{!confirmVisible ?  <IoEyeSharp /> :<BsEyeSlashFill /> }</span>
                 </div>
                 <button type='submit' className='w-full bg-[#089451] text-white font-bold py-2 my-2'>Sign Up</button>
