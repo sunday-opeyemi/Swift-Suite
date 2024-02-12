14aba9990f02c4ca3005c0127575e57629a55037
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-02-12 16:17:22 +0100

    registration success design

---
+++ frontend/src/pages/Regsuccess.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import sucImage from '../Images/suc.png'
+import Navbar from '../components/Navbar'
+
+const Regsuccess = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="flex flex-col items-center justify-center  h-[85vh]" >
+      <div className='my-2'>
+            <img src={sucImage} alt=""  width={150}/>
+      </div>
+      <div className='my-4 font-bold'>
+        <p>Registration Successful</p>
+      </div>
+      <div className='my-2  lg:w-[280px] md:w-[200px] w-[180px]'>
+        <button className='bg-[#089451]  text-white font-bold py-1 w-full rounded'>Sign In</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Regsuccess
