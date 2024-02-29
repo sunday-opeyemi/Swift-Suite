e6f78b278df59c90468a762ac4292c91c88f8548
Sunday Opeyemi <sunday.opeyemi2018@gmail.com>
2024-02-29 09:13:16 +0100

    I have added more vendor to the model

--- backend/swiftsuite/vendorApp/views.py
+++ backend/swiftsuite/vendorApp/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from ftplib import FTP
 import time, os
 import csv
-from .models import Fragrancex
+from .models import Fragrancex, Lipsey
 
 # Create your views here.
-
+supplier_name
 def download_csv_from_ftp(ftp_host, ftp_user, ftp_password, ftp_path, file_name, local_dir="."):
     """Download CSV file from FTP server."""
     try:
@@ -20,33 +20,51 @@ def download_csv_from_ftp(ftp_host, ftp_user, ftp_password, ftp_path, file_name,
         with open(os.path.join(local_dir, file_name), "r", encoding='utf-8') as file:
             csv_data = csv.DictReader(file)
             insert_data = []
-            for row in csv_data:
-                insert_data.append(Fragrancex(item=row["ITEM"], name=row["NAME"], description=row["DESCRIPTION"], 
-                    brand=row["BRAND"], title=row["TITLE"], gender=row["GENDER"], 
-                    size=row["Size"], metric_size=row["Metric_Size"], retail=row["RETAIL"], 
-                    price=row["PRICE"], eur_price=row["EUR_PRICE"],gbp_price=row["GBP_PRICE"], 
-                    cad_price=row["CAD_PRICE"], aud_price=row["AUD_PRICE"], image=row["IMAGE"], 
-                    url=row["URL"], qty=row["QTY"], upc=row["UPC"]))
-            Fragrancex.objects.bulk_create(insert_data)
-                
+            if supplier_name == "Fragrancex":
+                for row in csv_data:
+                    insert_data.append(Fragrancex(user_id=request.user.id, name=row["NAME"], item=row["ITEM"], description=row["DESCRIPTION"], brand=row["BRAND"], title=row["TITLE"], gender=row["GENDER"], size=row["Size"], metric_size=row["Metric_Size"], retail=row["RETAIL"], price=row["PRICE"], eur_price=row["EUR_PRICE"],gbp_price=row["GBP_PRICE"], cad_price=row["CAD_PRICE"], aud_price=row["AUD_PRICE"], image=row["IMAGE"], url=row["URL"], qty=row["QTY"], upc=row["UPC"]))
+                Fragrancex.objects.bulk_create(insert_data)
+            elif supplier_name == "Lipsey":
+                for row in csv_data:
+                    insert_data.append(Lipsey(user_id=request.user.id,itemnumber=row["itemnumber"], name=row["NAME"], description=row["DESCRIPTION"], 
+                        brand=row["BRAND"], title=row["TITLE"], gender=row["GENDER"], 
+                        size=row["Size"], metric_size=row["Metric_Size"], retail=row["RETAIL"], 
+                        price=row["PRICE"], eur_price=row["EUR_PRICE"],gbp_price=row["GBP_PRICE"], 
+                        cad_price=row["CAD_PRICE"], aud_price=row["AUD_PRICE"], image=row["IMAGE"], 
+                        url=row["URL"], qty=row["QTY"], upc=row["UPC"]))
+                Fragrancex.objects.bulk_create(insert_data)
+            elif supplier_name == "SSI":
+                for row in csv_data:
+                    pass
+            elif supplier_name == "CWR":
+                for row in csv_data:
+                    pass
+            elif supplier_name == "RSR":
+                for row in csv_data:
+                    pass
+            elif supplier_name == "Zanders":
+                for row in csv_data:
+                    pass
     except Exception as e:
         print(f"Download {file_name} Error: {str(e)}")
 
 def process_supplier(supplier):
     """Process each supplier."""
+    global supplier_name
     supplier_name, ftp_host, ftp_user, ftp_password, ftp_path, file_name = supplier
     local_dir = os.path.join("local_dir", supplier_name)
+    supplier_name=supplier_name
     os.makedirs(local_dir, exist_ok=True)
     download_csv_from_ftp(ftp_host, ftp_user, ftp_password, ftp_path, file_name, local_dir)
 
 suppliers = [
     ("FragranceX", "ftp2.fragrancex.com", "frgx_temilolaoduola@gmail.com", "ftos3tpi", "/", "outgoingfeed_upc.csv"),
-    # ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "catalog.csv"),
-    # ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "accessoriespricingquantity.csv"),
-    # ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "pricingquantity.csv"),
-    # ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "accessoriescatalog.csv"),
-    # ("SSI", "www.rapidretail.net", "ssi_dot774rr", "Rapid_774!", "/Products", "RR_Products.csv"),
-    # ("SSI", "www.rapidretail.net", "ssi_dot774rr", "Rapid_774!", "/Pricing-Availability", "RR_Pricing_Availability.csv"),
+    ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "catalog.csv"),
+    ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "accessoriespricingquantity.csv"),
+    ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "pricingquantity.csv"),
+    ("Lipsey", "ftp.lipseysdistribution.net", "cat800459", "8b4c531467417ad97e5274d5ecfbc0eb", "/", "accessoriescatalog.csv"),
+    ("SSI", "www.rapidretail.net", "ssi_dot774rr", "Rapid_774!", "/Products", "RR_Products.csv"),
+    ("SSI", "www.rapidretail.net", "ssi_dot774rr", "Rapid_774!", "/Pricing-Availability", "RR_Pricing_Availability.csv"),
 ]
 
 def main():
