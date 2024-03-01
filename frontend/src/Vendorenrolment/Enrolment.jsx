9c968c213df24b969b7a5b1f89a0cf5a571b52c7
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-03-01 16:49:45 +0100

    commit

--- frontend/src/Vendorenrolment/Enrolment.jsx
+++ frontend/src/Vendorenrolment/Enrolment.jsx
@@ -33,20 +33,38 @@ const Enrolment = () => {
     }
   ])
   return (
-
-    <section className='bg-blue-100'>
+    <section className='bg-blue-100 h-full'>
     <div className='lg:ms-[20%] lg:me-[15%] flex lg:flex-row flex-col gap-8'>
       <div className='stepContainer border-2 w-[70%]'>
-
-    <section className='bg-blue-100 h-screen'>
-    <div className='lg:ms-[20%] lg:me-[15%] flex gap-8'>
-
-      <div className='stepContainer  w-[70%]'>
-
-
+        {currentIndex === 0 &&
+          (<Vendorenrolment />
+          )}
+        {currentIndex === 1 &&
+          (<Fpicredential  />
+          )}
+        {currentIndex === 2 &&
+          (<Fpioption />
+          )}
+        {currentIndex === 3 &&
+          (<Cataloguefilter />
+          )}
+          {currentIndex === 4 &&
+            (<Thank />
+            )}
+      </div>
       <div className='lg:w-[30%] md:w-[63%] w-[84%] md:ms-[21%] lg:ms-0 ms-7 border-2 lg:order-last bg-white -order-last mt-8 h-[30%] shadow'>
-
-      <div className='w-[30%] border-2  bg-white h-[30%] shadow'>
-
+            {
+              myList.map((items, index)=>(
+                <ul key={index} className='flex justify-between border-b border-gray-500 p-5'>
+                    <li className='font-semibold'>{items.name}</li>
+                    <li className={currentIndex >= index ? 'mt-2 text-[#089451]' : 'mt-2'}>{currentIndex >= index ? items.icon2 : items.icon}</li>
+                </ul>
+              ))
+            }
+      </div>
+    </div>
+    </section>
+  )
+}
 
 export default Enrolment
