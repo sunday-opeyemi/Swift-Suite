9cbf8904aedb33f14d7101cd84185628245b8c0a
Sunday <sunday.opeyemi2018@gmail.com>
2024-05-08 16:06:58 +0100

    make correction to zanders

--- backend/swiftsuite/vendorApp/views.py
+++ backend/swiftsuite/vendorApp/views.py
@@ -140,7 +140,6 @@ class VendorActivity:
                     # self.data = self.data[self.data['Quantity']>'22']    
                     for row in self.data.iterrows():
                         items = row[1].values   
-                        # print(items)
                         items = list(items)
                         insert_data.append(Cwr(user_id=userid, cwr_part_number=items[0], manufacturer_part_number=items[1], upc_code=items[2], quantity_available_to_ship_combined=items[3], quantity_available_to_ship_nj=items[4], quantity_available_to_ship_fl=items[5], next_shipment_date_combined=items[6], next_shipment_date_nj=items[7], next_shipment_date_fl=items[8], your_cost=items[9], list_price=items[10], m_a_p_price=items[11], m_r_p_price=items[12], uppercase_title=items[13], title=items[14], full_description=items[15], category_id=items[16], category_name=items[17], manufacturer_name=items[18], shipping_weight=items[19], box_height=items[20], box_length=items[21], box_width=items[22], list_of_accessories_by_sku=items[23], list_of_accessories_by_mfg=items[24], quick_specs=items[29], hazardous_materials=items[30], truck_freight=items[31], exportable=items[32], first_class_mail=items[33], oversized=items[34], remanufactured=items[35], closeout=items[36], harmonization_code=items[37], country_of_origin=items[38], sale=items[39], original_price_if_on_sale_closeout=items[40], sale_start_date=items[41], sale_end_date=items[42], rebate=items[43], rebate_description=items[44], rebate_start_date=items[45], rebate_end_date=items[46], google_merchant_category=items[47], quick_guide_literature_pdf_url=items[48], owners_manual_pdf_url=items[49], brochure_literature_pdf_url=items[50], installation_guide_pdf_url=items[51], video_urls=items[52], prop_65=items[53], prop_65_description=items[54], free_shipping=items[55], free_shipping_end_date=items[56], returnable=items[57], image_additional_1000x1000_urls=items[58], case_qty_nj=items[59], case_qty_fl=items[60], number_3rd_party_marketplaces=items[61], fcc_id=items[62], sku=items[63], mfgn=items[64], qty=items[65], qtynj=items[66], qtyfl=items[67], price=items[68], map=items[69], mrp=items[70]))
                     
@@ -161,20 +160,26 @@ class VendorActivity:
                         description.append(str(row).split("~")[2].replace("}", ""))
                     else:
                         items.append(row) 
+                
+                if index == 3:
+                    data2 = pd.DataFrame({"Itemnumber":itemNumber, "description":description})
+                    data = data.merge(data2, left_on="itemnumber", right_on="Itemnumber")
                     
-                data = data[data['ItemType']=='Firearm']
-                data = data[data['Quantity']>'22']    
-                for row in data.iterrows():
-                    items = row[1]      
-                    insert_data.append(Zanders(user_id=userid, available=row['available'], category=row['category'], desc1=row['desc1'], desc2=row['desc2'], itemnumber=row['itemnumber'], manufacturer=row['manufacturer'], mfgpnumber=row['mfgpnumber'], msrp=row['msrp'], price1=row['price1'], price2=row['price2'], price3=row['price3'], qty1=row['qty1'], qty2=row['qty2'], qty3=row['qty3'], upc=row['upc'], weight=row['weight'], serialized=row['serialized'], mapprice=row['mapprice'], imagelink=row['ImageLink'], description=["description"]))       
-                    
- 
-                print(len(insert_data))                  
-                Zanders.objects.bulk_create(insert_data, batch_size=500, update_conflicts=True, update_fields=["price1", "price2", "price3"])
-                print('\nProduct upload successful....')
+                    # data = data[data['ItemType']=='Firearm']
+                    # data = data[data['Quantity']>'22']    
+                    for row in data.iterrows():
+                        items = row[1]      
+                        insert_data.append(Zanders(user_id=userid, available=row['available'], category=row['category'], desc1=row['desc1'], desc2=row['desc2'], itemnumber=row['itemnumber'], manufacturer=row['manufacturer'], mfgpnumber=row['mfgpnumber'], msrp=row['msrp'], price1=row['price1'], price2=row['price2'], price3=row['price3'], qty1=row['qty1'], qty2=row['qty2'], qty3=row['qty3'], upc=row['upc'], weight=row['weight'], serialized=row['serialized'], mapprice=row['mapprice'], imagelink=row['ImageLink'], description=["description"]))                           
 
+                    print(len(insert_data))                  
+                    Zanders.objects.bulk_create(insert_data, batch_size=500, update_conflicts=True, update_fields=["price1", "price2", "price3"])
+                    print('\nProduct upload successful....')
+                elif index == 2:
+                    data2 = pd.DataFrame(items)
+                    data = data.merge(data2, left_on="itemnumber", right_on="ItemNumber")
+                else:
+                    data = pd.DataFrame(items) 
 
-                                      
 
 VENDORS = {
     "fragrancex":[
