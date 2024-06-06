58cc9839819f4e163b16645c7db70cd49577b252
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-06-06 11:16:46 +0100

    commit

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import gif from '../Images/gif.gif'
 
 const Product = () => {
+  const store = useSelector((state) => state.vendor.productId);
+  console.log(store);
+
+  const productId = JSON.parse(localStorage.getItem('productId'))
+  console.log(productId);
+
+  const token = JSON.parse(localStorage.getItem('token'))
+  console.log(token);
+
+  const [loader, setLoader] = useState(true)
+
+
+  const [userProduct, setUserProduct] = useState([])
+
+  let endpoint = 'https://service.swiftsuite.app/vendor/view-all-products/46/'
+
+  useEffect(() => {
+    axios.get(endpoint, {
+      headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },})
+    .then((result)=>{
+      console.log(result.data);
+      setUserProduct(result.data)
+      setLoader(false)
+    })
+    .catch((err)=>{
+      console.log(err);
+      setLoader(false)
+    })
+  }, [])
+  
   return (
   
-    <section className='bg-green-50 h-screen'>
+    <section className='bg-green-50 py-[5%]'>
     <div className='lg:ms-[24%]'>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quas libero, cumque sunt ipsum quam tempora saepe laudantium temporibus explicabo minus, qui voluptas veritatis ex odio inventore. Obcaecati atque dolores laboriosam officia delectus asperiores architecto perferendis, corrupti quisquam porro qui. Dolor, quibusdam. Aut hic sint harum modi, totam, iste tempore cum, libero consectetur rem ipsam vero obcaecati vel sunt culpa recusandae eius? Cupiditate non earum dolor at reprehenderit esse aliquid cum voluptate voluptatibus, itaque nisi quas recusandae, aperiam et. Facilis laboriosam, modi natus impedit obcaecati accusantium possimus odit hic iure quos incidunt iusto velit numquam explicabo fuga suscipit placeat nobis?
+      
+      <>
+      {loader && (
+          <div className="flex justify-center items-center">
+            <img
+              src={gif}
+              alt="Loading..."
+              className="lg:ms-[-100px] border p-3 shadow-xl rounded-xl w-[50px] mt-10"
+            />
+          </div>
+        )}
+      {userProduct.length > 0 ? (
+          userProduct.map((item, i) => (
+            <div key={i} className='grid grid-cols-3 shadow-xl cursor-pointer rounded-xl bg-white mb-10 p-3'>
+              <div>
+              <img src={`https://www.lipseyscloud.com/images/${item.image}`} alt={item.image} className="w-[40%] mt-10  justify-center items-center rounded-md mx-auto object-cover"/>
+              </div>
+              <div>
+              <p>ID: {item.id}</p>
+              {/* <p>TITLE: {item.title}</p> */}
+              <p>UPC: {item.upc}</p>
+              <p>SKU: {item.sku}</p>
+              {/* <p>MODEL: {item.model}</p> */}
+              <p>PRICE: {item.price}</p>
+              {/* {/* <p>QUANTITY: {item.quantity}</p> */}
+              <p>MSRP: {item.msrp}</p>
+              </div>
+              <div>
+              <p>MPN: {item.mpn}</p>
+              <p>BRAND: {item.brand}</p>
+              <p>CATEGORY: {item.category}</p>
+              <p>DESCRIPTION: {item.detailed_description}</p>
+              <p>SHIPPING WEIGHT: {item.shipping_weight}</p>
+              {/* <p>SHIPPING HEIGHT: {item.shipping_height}</p>
+              <p>SHIPPING WIDTH: {item.shipping_width}</p>
+              <p>USER: {item.user}</p> */}
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className='text-black bg-green-50 h-screen text-xl lg:ms-[100px] w-[90%]'>No products available.</p>
+        )
+        }
+      </>
     </div>
     </section>
    
